1519cd0b6a79af49278d5c2485e485fe7d5618a3
gg <yifang-chen@yifang-chendeMacBook-Pro.local>
2021-08-22 15:50:52 +0800

    Bug fixed and imporved user experience.

---
+++ gcm&lcd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm> 
+
+using namespace std;
+const int long MOD = 1000000007;
+unsigned long long int FINAL = 0;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i <= n; i++) {
+        FINAL += 
+    }
+    return 0;
+}
